a957fe5c3968692651e564367cfea6a0a6ce9c68
Alana Alexander <laninsky@gmail.com>
2018-03-05 15:59:27 +1300

    Update mito_shell.sh

--- mito_shell.sh
+++ mito_shell.sh
@@ -3,6 +3,11 @@ samplesfilepath=`tail -n+2 mito_shell_params.txt | head -n1`;
 maxmemory=`tail -n+3 mito_shell_params.txt | head -n1`;
 numbercores=`tail -n+4 mito_shell_params.txt | head -n1`;
 
+numberofsamples=`wc -l $samplesfilepath | awk '{print $1}'`
 
-
-$trinitypath --seqType fq --max_memory $maxmemory --samples_file $samplesfilepath --CPU $numbercores --full_cleanup
+for i in `seq 1 $numberofsamples`; 
+do samplename=`tail -n+$i $samplesfilepath | head -n1 | awk '{print $1}'`;
+left=`tail -n+$i $samplesfilepath | head -n1 | awk '{print $2}'`;
+right=`tail -n+$i $samplesfilepath | head -n1 | awk '{print $3}'`;
+$trinitypath --seqType fq --max_memory $maxmemory --left $left --right $right --CPU $numbercores --output  ${samplename}_trinity_out_dir --full_cleanup
+done
