3315f5ca304eb1dd8b456f1cab3652dad67aabfe
Alana Alexander <laninsky@gmail.com>
2018-03-06 10:32:16 +1300

    Create 2_blast_fishing.sh

---
+++ 2_blast_fishing/2_blast_fishing.sh
@@ -0,0 +1,16 @@
+makeblastdb=`tail -n+5 mito_shell_params.txt | head -n1`;
+blastn=`tail -n+6 mito_shell_params.txt | head -n1`;
+seqtk=`tail -n+7 mito_shell_params.txt | head -n1`;
+step1fasta=`tail -n+8 mito_shell_params.txt | head -n1`;
+reference=`tail -n+9 mito_shell_params.txt | head -n1`;
+blastperc=`tail -n+10 mito_shell_params.txt | head -n1`;
+
+numberofsamples=`wc -l $samplesfilepath | awk '{print $1}'`
+
+for i in `seq 1 $numberofsamples`; 
+do samplename=`tail -n+$i $samplesfilepath | head -n1 | awk '{print $1}'`;
+left=`tail -n+$i $samplesfilepath | head -n1 | awk '{print $2}'`;
+right=`tail -n+$i $samplesfilepath | head -n1 | awk '{print $3}'`;
+$trinitypath --seqType fq --max_memory $maxmemory --left $left --right $right --CPU $numbercores --output  ${samplename}_trinity_out_dir --full_cleanup >> 1_denovo.log
+rm ${samplename}_trinity_out_dir.fasta.gene_trans_map;
+done
