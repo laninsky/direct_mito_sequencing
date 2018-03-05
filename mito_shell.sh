47ebff45a1cba09c95d21d34217c7bdfc3da87ab
Alana Alexander <laninsky@gmail.com>
2018-03-05 14:27:48 +1300

    Update mito_shell.sh

--- mito_shell.sh
+++ mito_shell.sh
@@ -1,10 +1,8 @@
-trinitypath=`tail -n+1 phasing_settings | head -n1`;
-samplesfilepath=`tail -n+2 phasing_settings | head -n1`;
-numbercores=`tail -n+3 phasing_settings | head -n1`;
-
-
-sequencing=`tail -n+4 phasing_settings | head -n1`;
+trinitypath=`tail -n+1 mito_shell_params.txt | head -n1`;
+samplesfilepath=`tail -n+2 mito_shell_params.txt | head -n1`;
+maxmemory=`tail -n+3 mito_shell_params.txt | head -n1`;
+numbercores=`tail -n+4 mito_shell_params.txt | head -n1`;
 
 
 
-$trinitypath --seqType fq --max_memory 200GB --samples_file $samplesfilepath -
+$trinitypath --seqType fq --max_memory $maxmemory --samples_file $samplesfilepath --CPU $numbercores --full_cleanup
