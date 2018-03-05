faca7278c5fd52d7676615aa76981d0b4dfd500e
Alana Alexander <laninsky@gmail.com>
2018-03-05 14:25:04 +1300

    Update mito_shell.sh

--- mito_shell.sh
+++ mito_shell.sh
@@ -1,3 +1,10 @@
+trinitypath=`tail -n+1 phasing_settings | head -n1`;
+samplesfilepath=`tail -n+2 phasing_settings | head -n1`;
+numbercores=`tail -n+3 phasing_settings | head -n1`;
 
 
-Trinity --seqType fq --max_memory 200GB --samples_file samples_file.txt -
+sequencing=`tail -n+4 phasing_settings | head -n1`;
+
+
+
+$trinitypath --seqType fq --max_memory 200GB --samples_file $samplesfilepath -
