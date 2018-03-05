da09191ebfe07b4024add7535eab129c1ab89ce0
Alana Alexander <laninsky@gmail.com>
2018-03-06 12:56:10 +1300

    Update 2_blast_fishing.sh

--- 2_blast_fishing/2_blast_fishing.sh
+++ 2_blast_fishing/2_blast_fishing.sh
@@ -5,7 +5,24 @@ step1fasta=`tail -n+8 mito_shell_params.txt | head -n1`;
 reference=`tail -n+9 mito_shell_params.txt | head -n1`;
 blastperc=`tail -n+10 mito_shell_params.txt | head -n1`;
 
-numberofsamples=`wc -l $samplesfilepath | awk '{print $1}'`
+if [ "$step1fasta" == "Step_1" ];
+then 
+samplesfilepath=`tail -n+2 mito_shell_params.txt | head -n1`;
+numberofsamples=`wc -l $samplesfilepath | awk '{print $1}'`;
+for i in `seq 1 $numberofsamples`; 
+do samplename=`tail -n+$i $samplesfilepath | head -n1 | awk '{print $1}'`;
+
+
+
+else
+numberofsamples=`wc -l $step1fasta | awk '{print $1}'`;
+for i in `seq 1 $numberofsamples`; 
+do samplename=`tail -n+$i $step1fasta | head -n1 | awk '{print $1}'`;
+
+
+fi
+
+
 
 for i in `seq 1 $numberofsamples`; 
 do samplename=`tail -n+$i $samplesfilepath | head -n1 | awk '{print $1}'`;
