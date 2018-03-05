3cd42e594b0dfacf7e55da03a59f000d87127580
Alana Alexander <laninsky@gmail.com>
2018-03-05 16:44:35 +1300

    Update 1_denovo.sh

--- 1_denovo/1_denovo.sh
+++ 1_denovo/1_denovo.sh
@@ -9,5 +9,5 @@ for i in `seq 1 $numberofsamples`;
 do samplename=`tail -n+$i $samplesfilepath | head -n1 | awk '{print $1}'`;
 left=`tail -n+$i $samplesfilepath | head -n1 | awk '{print $2}'`;
 right=`tail -n+$i $samplesfilepath | head -n1 | awk '{print $3}'`;
-$trinitypath --seqType fq --max_memory $maxmemory --left $left --right $right --CPU $numbercores --output  ${samplename}_trinity_out_dir --full_cleanup
+$trinitypath --seqType fq --max_memory $maxmemory --left $left --right $right --CPU $numbercores --output  ${samplename}_trinity_out_dir --full_cleanup >> 1_denovo.log
 done
