495ee0a86f3d95776f1f04353db372dd2d2a5ae1
Alana Alexander <laninsky@gmail.com>
2018-03-06 16:01:39 +1300

    Update 2_blast_fishing.sh

--- 2_blast_fishing/2_blast_fishing.sh
+++ 2_blast_fishing/2_blast_fishing.sh
@@ -5,29 +5,25 @@ step1fasta=`tail -n+8 mito_shell_params.txt | head -n1`;
 reference=`tail -n+9 mito_shell_params.txt | head -n1`;
 blastperc=`tail -n+10 mito_shell_params.txt | head -n1`;
 
+grep -v '^$' $reference > reference_no_blank_lines.fasta
+$makeblastdb -in reference_no_blank_lines.fasta -dbtype nucl
+
 if [ "$step1fasta" == "Step_1" ];
 then 
 samplesfilepath=`tail -n+2 mito_shell_params.txt | head -n1`;
 numberofsamples=`wc -l $samplesfilepath | awk '{print $1}'`;
 for i in `seq 1 $numberofsamples`; 
 do samplename=`tail -n+$i $samplesfilepath | head -n1 | awk '{print $1}'`;
+$blastn -db reference_no_blank_lines.fasta -query ${samplename}_trinity_out_dir.Trinity.fasta -perc_identity $blastperc -outfmt '10 qseqid sseqid qlen slen' | cut -d , -f 1 | uniq > $samplename.namelist;
 
-
+done
 
 else
 numberofsamples=`wc -l $step1fasta | awk '{print $1}'`;
 for i in `seq 1 $numberofsamples`; 
 do samplename=`tail -n+$i $step1fasta | head -n1 | awk '{print $1}'`;
+samplefasta=`tail -n+$i $step1fasta | head -n1 | awk '{print $2}'`
+$blastn -db reference_no_blank_lines.fasta -query $samplefasta -perc_identity $blastperc -outfmt '10 qseqid sseqid qlen slen' | cut -d , -f 1 | uniq > $samplename.namelist;
 
-
-fi
-
-
-
-for i in `seq 1 $numberofsamples`; 
-do samplename=`tail -n+$i $samplesfilepath | head -n1 | awk '{print $1}'`;
-left=`tail -n+$i $samplesfilepath | head -n1 | awk '{print $2}'`;
-right=`tail -n+$i $samplesfilepath | head -n1 | awk '{print $3}'`;
-$trinitypath --seqType fq --max_memory $maxmemory --left $left --right $right --CPU $numbercores --output  ${samplename}_trinity_out_dir --full_cleanup >> 1_denovo.log
-rm ${samplename}_trinity_out_dir.fasta.gene_trans_map;
 done
+fi
