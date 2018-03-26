a00ea3ae534f4dc96daff7c9352643cbd3291456
Alana Alexander <laninsky@gmail.com>
2018-03-26 16:18:57 +1300

    Update 3_reference_mapping.sh

--- 3_reference_mapping/3_reference_mapping.sh
+++ 3_reference_mapping/3_reference_mapping.sh
@@ -6,9 +6,30 @@ minread=`tail -n+4 ref_map_params.txt | head -n1`;
 numberofsamples=`wc -l $refmappath | awk '{print $1}'`
 
 for i in `seq 1 $numberofsamples`; 
-do refname=`tail -n+$i $refmappath | head -n1 | awk '{print $1}'`;
-left=`tail -n+$i $refmappath | head -n1 | awk '{print $2}'`;
-right=`tail -n+$i $refmappath | head -n1 | awk '{print $3}'`;
+do refname=`tail -n+$i $refmappath | head -n1 | awk '{print $2}'`;
+left=`tail -n+$i $refmappath | head -n1 | awk '{print $3}'`;
+right=`tail -n+$i $refmappath | head -n1 | awk '{print $4}'`;
+assemblyname=`tail -n+$i $refmappath | head -n1 | awk '{print $1}'`;
+bwa index -a is $refname
+samtools faidx $refname 
+picarddict=`echo $refname | sed 's/.fasta/.dict/g'`
+java -jar $picard CreateSequenceDictionary R=$refname O=$picarddict
+bwa mem $refname $left $right > temp.sam
+
+/public/jdk1.8.0_112/bin/java -jar /public/picard.jar AddOrReplaceReadGroups I=temp.sam O=tempsort.sam SORT_ORDER=coordinate LB=rglib PL=illumina PU=phase SM=everyone
+/public/jdk1.8.0_112/bin/java -jar /public/picard.jar MarkDuplicates MAX_FILE_HANDLES=1000 I=tempsort.sam O=tempsortmarked.sam M=temp.metrics AS=TRUE
+/public/jdk1.8.0_112/bin/java -jar /public/picard.jar SamFormatConverter I=tempsortmarked.sam O=tempsortmarked.bam
+samtools index tempsortmarked.bam
+gatk -T RealignerTargetCreator -R sle117_final_mitobim.fasta -I tempsortmarked.bam -o tempintervals.list
+gatk  -T IndelRealigner -R sle117_final_mitobim.fasta -I tempsortmarked.bam -targetIntervals tempintervals.list -o temp_realigned_reads.bam
+gatk -T DepthOfCoverage -R sle117_final_mitobim.fasta -I temp_realigned_reads.bam -o temp.coverage
+
+gatk -T HaplotypeCaller -R sle117_final_mitobim.fasta -I temp_realigned_reads.bam --genotyping_mode DISCOVERY -stand_call_conf 30 -o temp_raw_variants.vcf --maxNumHaplotypesInPopulation 1
+gatk -T ReadBackedPhasing -R sle117_final_mitobim.fasta -I temp_realigned_reads.bam  --variant temp_raw_variants.vcf -o temp_phased_SNPs.vcf
+gatk -T FindCoveredIntervals -R sle117_final_mitobim.fasta -I temp_realigned_reads.bam -cov 4 -o temp_covered.list
+gatk -T FastaAlternateReferenceMaker -V temp_phased_SNPs.vcf -R sle117_final_mitobim.fasta -L temp_covered.list -o sle117_final_mapped.fasta
+mv temp.coverage coverage.txt
+rm temp*
 
 
 $trinitypath --seqType fq --max_memory $maxmemory --left $left --right $right --CPU $numbercores --output  ${samplename}_trinity_out_dir --full_cleanup >> 1_denovo.log
