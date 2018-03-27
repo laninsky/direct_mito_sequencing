7fb485a7fa7a60767a5c9231335a9ecfd74c3905
Alana Alexander <laninsky@gmail.com>
2018-03-27 13:52:09 +1300

    Update 3_reference_mapping.sh

--- 3_reference_mapping/3_reference_mapping.sh
+++ 3_reference_mapping/3_reference_mapping.sh
@@ -2,6 +2,7 @@ refmappath=`tail -n+1 ref_map_params.txt | head -n1`;
 picard=`tail -n+2 ref_map_params.txt | head -n1`;
 numbercores=`tail -n+3 ref_map_params.txt | head -n1`;
 minread=`tail -n+4 ref_map_params.txt | head -n1`;
+gatk38=`tail -n+5 ref_map_params.txt | head -n1`;
 
 numberofsamples=`wc -l $refmappath | awk '{print $1}'`
 
@@ -20,13 +21,22 @@ java -jar $picard MarkDuplicates MAX_FILE_HANDLES=1000 I=tempsort.sam O=tempsort
 java -jar $picard SamFormatConverter I=tempsortmarked.sam O=tempsortmarked.bam
 samtools index tempsortmarked.bam
 gatk HaplotypeCaller -R $refname -I tempsortmarked.bam -stand-call-conf 30 -O temp_raw_variants.vcf --max-num-haplotypes-in-population 1
-
-
-gatk FindCoveredIntervals -R $refname -I tempsortmarked.bam -cov $minread -O temp_covered.list
-
-
-gatk FastaAlternateReferenceMaker -V temp_raw_variants.vcf -R sle117_final_mitobim.fasta -L temp_covered.list -o $assemblyname.fasta
-samtools pileup -f sle117_final_mitobim.fasta -l temp_covered.list temp_realigned_reads.bam > $assemblyname.pileup
+java -jar $gatk38 -T FindCoveredIntervals -R $refname -I tempsortmarked.bam -cov $minread -o temp_covered.list
+java -jar $gatk38 -T FastaAlternateReferenceMaker -V temp_raw_variants.vcf -R $refname -L temp_covered.list -o $assemblyname.fasta
+rm temp*
+bwa index -a is $assemblyname.fasta
+samtools faidx $assemblyname.fasta
+picarddict=`echo $assemblyname.fasta | sed 's/.fasta/.dict/g'`
+java -jar $picard CreateSequenceDictionary R=$assemblyname.fasta O=$picarddict
+bwa mem -t $numbercores $assemblyname.fasta $left $right > temp.sam
+java -jar $picard AddOrReplaceReadGroups I=temp.sam O=tempsort.sam SORT_ORDER=coordinate LB=rglib PL=illumina PU=phase SM=everyone
+java -jar $picard MarkDuplicates MAX_FILE_HANDLES=1000 I=tempsort.sam O=tempsortmarked.sam M=temp.metrics AS=TRUE
+java -jar $picard SamFormatConverter I=tempsortmarked.sam O=tempsortmarked.bam
+samtools index tempsortmarked.bam
+java -jar $gatk38 -T FindCoveredIntervals -R $assemblyname.fasta -I tempsortmarked.bam -cov $minread -o temp_covered.list
+sed  's/:\|-/\t/gi' temp_covered.list > test.bed
+samtools mpileup -f $assemblyname.fasta -l temp.bed tempsortmarked.bam > $assemblyname.pileup
+rm temp*
 
 rm -rf *.fai
 rm -rf *.sa
@@ -35,10 +45,4 @@ rm -rf *.ann
 rm -rf *.bwt
 rm -rf *.pac
 
-mv temp.coverage coverage.txt
-rm temp*
-
-
-$trinitypath --seqType fq --max_memory $maxmemory --left $left --right $right --CPU $numbercores --output  ${samplename}_trinity_out_dir --full_cleanup >> 1_denovo.log
-rm ${samplename}_trinity_out_dir.fasta.gene_trans_map;
 done
